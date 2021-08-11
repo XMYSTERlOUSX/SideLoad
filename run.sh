64b64eb96ce043a9d9df707665bc7b60fa87271f
XMYSTERlOUSX <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-08-11 13:46:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo "Cloning the repo..."
 # clone the repo
-git clone https://github.com/XMYSTERlOUSX/mega-link-downloader-bot/
+git clone https://github.com/XMYSTERlOUSX/mega-test/
 git clone https://megous.com/git/megatools
 
 echo "Cloned Successfully."
